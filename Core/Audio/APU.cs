e51710ffec642d8380265fd88aa26573470c71a5
adrigg2 <adriangarcia2.agg@gmail.com>
2026-07-22 02:06:28 +0200

    first implementation of audio

--- Core/Audio/APU.cs
+++ Core/Audio/APU.cs
@@ -1,15 +1,26 @@
-﻿namespace GameBoyEmulator.Core.Audio;
+﻿using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using System.Printing;
+
+namespace GameBoyEmulator.Core.Audio;
 
 public class APU
 {
+    private const float CyclesPerSample = 4194304.0f / 44100.0f;
+
     private byte _nr50;
     private byte _nr51;
     private byte _nr52;
+
+    private float _sampleCycles;
 
     private Channel1 _channel1;
     private Channel2 _channel2;
     private Channel3 _channel3;
     private Channel4 _channel4;
+
+    private APUSampleProvider _waveProvider;
+    private WasapiOut _out;
     
     public byte NR50 { get => _nr50; set => _nr50 = value; }
     public byte NR51 { get => _nr51; set => _nr51 = value; }
@@ -67,16 +78,24 @@ public class APU
         _channel2 = new();
         _channel3 = new();
         _channel4 = new();
+
+        var format = WaveFormat.CreateIeeeFloatWaveFormat(44100, 2);
+        _waveProvider = new APUSampleProvider(format);
+        _out = new WasapiOut(AudioClientShareMode.Shared, 50);
+        _out.Init(_waveProvider);
+        _out.Play();
     }
 
     public void Tick(int cycles, int divApuCounter)
     {
+        _sampleCycles += cycles;
+
         if (divApuCounter % 2 == 0)
         {
             _channel1.LengthTimer();
             _channel2.LengthTimer();
             _channel3.LengthTimer();
-            // sound length
+            _channel4.LengthTimer();
         }
 
         if (divApuCounter % 4 == 0)
@@ -88,12 +107,72 @@ public class APU
         {
             _channel1.EnvelopeSweep();
             _channel2.EnvelopeSweep();
-            // envelope sweep
+            _channel4.EnvelopeSweep();
         }
 
         _channel1.Tick(cycles);
         _channel2.Tick(cycles);
         _channel3.Tick(cycles);
         _channel4.Tick(cycles);
+
+        if (_sampleCycles >= CyclesPerSample)
+        {
+            _sampleCycles -= CyclesPerSample;
+            GenerateSample();
+        }
+    }
+
+    private void GenerateSample()
+    {
+        float left = 0;
+        float right = 0;
+
+        if ((_nr51 & 0x80) != 0)
+        {
+            left += _channel4.Output;
+        }
+
+        if ((_nr51 & 0x40) != 0)
+        {
+            left += _channel3.Output;
+        }
+
+        if ((_nr51 & 0x20) != 0)
+        {
+            left += _channel2.Output;
+        }
+
+        if ((_nr51 & 0x10) != 0)
+        {
+            left += _channel1.Output;
+        }
+
+        if ((_nr51 & 0x08) != 0)
+        {
+            right += _channel4.Output;
+        }
+
+        if ((_nr51 & 0x04) != 0)
+        {
+            right += _channel3.Output;
+        }
+
+        if ((_nr51 & 0x02) != 0)
+        {
+            right += _channel2.Output;
+        }
+
+        if ((_nr51 & 0x01) != 0)
+        {
+            right += _channel1.Output;
+        }
+
+        float volRight = ((_nr50 & 0x07) + 1) / 8.0f;
+        float volLeft = (((_nr50 & 0x70) >> 4) + 1) / 8.0f;
+
+        left = (left / 4) * volLeft;
+        right = (right / 4) * volRight;
+
+        _waveProvider.WriteSample((left + right) / 2);
     }
 }
---
+++ Core/Audio/APUSampleProvider.cs
@@ -0,0 +1,41 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBoyEmulator.Core.Audio;
+
+public class APUSampleProvider : ISampleProvider
+{
+    private readonly ConcurrentQueue<float> _ringBuffer;
+    public WaveFormat WaveFormat { get; }
+
+    public APUSampleProvider(WaveFormat waveFormat)
+    {
+        WaveFormat = waveFormat;
+        _ringBuffer = new();
+    }
+
+    public int Read(float[] buffer, int offset, int count)
+    {
+        int written = 0;
+        while (written < count && _ringBuffer.TryDequeue(out float sample))
+        {
+            buffer[offset + written++] = sample;
+        }
+
+        while (written < count)
+        {
+            buffer[offset + written++] = 0f;
+        }
+        return count;
+    }
+
+    public void WriteSample(float sample)
+    {
+        _ringBuffer.Enqueue(sample);
+    }
+}
